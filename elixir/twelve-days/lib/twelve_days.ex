373dfa5f5aa32cb2ed8aeadf0a23e70c38a278dc
enricoschaaf <enricoschaaf98@gmail.com>
2020-11-14 16:30:01 +0100

    Twelve days side exersize solution

--- elixir/twelve-days/lib/twelve_days.ex
+++ elixir/twelve-days/lib/twelve_days.ex
@@ -1,10 +1,52 @@
 defmodule TwelveDays do
+  @days %{
+    1 => "first",
+    2 => "second",
+    3 => "third",
+    4 => "fourth",
+    5 => "fifth",
+    6 => "sixth",
+    7 => "seventh",
+    8 => "eighth",
+    9 => "ninth",
+    10 => "tenth",
+    11 => "eleventh",
+    12 => "twelfth"
+  }
+
+  @verses %{
+    1 => "a Partridge in a Pear Tree",
+    2 => "two Turtle Doves",
+    3 => "three French Hens",
+    4 => "four Calling Birds",
+    5 => "five Gold Rings",
+    6 => "six Geese-a-Laying",
+    7 => "seven Swans-a-Swimming",
+    8 => "eight Maids-a-Milking",
+    9 => "nine Ladies Dancing",
+    10 => "ten Lords-a-Leaping",
+    11 => "eleven Pipers Piping",
+    12 => "twelve Drummers Drumming"
+  }
+
   @doc """
   Given a `number`, return the song's verse for that specific day, including
   all gifts for previous days in the same line.
   """
   @spec verse(number :: integer) :: String.t()
+
+  def verse(1) do
+    "On the #{Map.get(@days, 1)} day of Christmas my true love gave to me: #{Map.get(@verses, 1)}."
+  end
+
   def verse(number) do
+    verses =
+      number..2
+      |> Enum.map_join(", ", &Map.get(@verses, &1))
+
+    "On the #{Map.get(@days, number)} day of Christmas my true love gave to me: #{verses}, and #{
+      Map.get(@verses, 1)
+    }."
   end
 
   @doc """
@@ -13,6 +55,7 @@ defmodule TwelveDays do
   """
   @spec verses(starting_verse :: integer, ending_verse :: integer) :: String.t()
   def verses(starting_verse, ending_verse) do
+    starting_verse..ending_verse |> Enum.map_join("\n", &verse/1)
   end
 
   @doc """
@@ -20,5 +63,6 @@ defmodule TwelveDays do
   """
   @spec sing() :: String.t()
   def sing do
+    verses(1, 12)
   end
 end
