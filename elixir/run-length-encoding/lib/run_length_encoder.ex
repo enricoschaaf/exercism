6c9ffa1c078232c6111f388177000a104f3b3273
enricoschaaf <enricoschaaf98@gmail.com>
2020-11-14 13:15:08 +0100

    Run Length Encoder side exercise

--- elixir/run-length-encoding/lib/run_length_encoder.ex
+++ elixir/run-length-encoding/lib/run_length_encoder.ex
@@ -13,10 +13,43 @@ defmodule RunLengthEncoder do
   end
 
   def encode(string) do
-    string |> String.codepoints() |> List.zip()
+    string
+    |> String.codepoints()
+    |> Enum.chunk_by(& &1)
+    |> Enum.map_join(&construct/1)
+  end
+
+  defp construct([head | []]) do
+    head
+  end
+
+  defp construct([head | tail]) do
+    "#{Enum.count(tail) + 1}#{head}"
   end
 
   @spec decode(String.t()) :: String.t()
+
+  def decode("") do
+    ""
+  end
+
   def decode(string) do
+    string
+    |> String.split(~r/\d+|[a-zA-Z]/, include_captures: true, trim: true)
+    |> reconstruct()
+  end
+
+  defp reconstruct([head | []]), do: head
+
+  defp reconstruct([head | tail]) do
+    match(head, tail) <> reconstruct(tail)
+  end
+
+  defp match(head, tail) do
+    if String.match?(head, ~r/\d/) do
+      String.duplicate(List.first(tail), String.to_integer(head) - 1)
+    else
+      head
+    end
   end
 end
