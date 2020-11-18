6976d2e257f756330520b096b21e8dd04f0a7139
enricoschaaf <enricoschaaf98@gmail.com>
2020-11-18 04:22:00 +0100

    Learnt something new about regex

--- elixir/word-count/lib/word_count.ex
+++ elixir/word-count/lib/word_count.ex
@@ -8,8 +8,7 @@ defmodule WordCount do
   def count(sentence) do
     sentence
     |> String.downcase()
-    |> String.replace(~w(: ! & @ $ % ^ , _), " ")
-    |> String.split()
+    |> String.split(~r/[^[:alnum:]-]/u, trim: true)
     |> Enum.frequencies()
   end
 end
