2a7c860de4deb1f3fa63106646d418e9966ce065
enricoschaaf <enricoschaaf98@gmail.com>
2020-11-18 23:35:28 +0100

    Rust Gigasecond task

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,8 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, NaiveDateTime, Utc};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    unimplemented!("What time is a gigasecond later than {}", start);
+    let seconds = DateTime::<Utc>::from(start).timestamp() + 1_000_000_000;
+    let native_date_time = NaiveDateTime::from_timestamp(seconds, 0);
+    return DateTime::<Utc>::from_utc(native_date_time, Utc);
 }
