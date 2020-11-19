1f5434b8590e24ee41f93db5099b47505a0f188f
enricoschaaf <enricoschaaf98@gmail.com>
2020-11-19 05:19:22 +0100

    Improve Solution

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,8 +1,7 @@
-use chrono::{DateTime, NaiveDateTime, Utc};
+use chrono::{DateTime, Duration, Utc};
 
 // Returns a Utc DateTime one billion seconds after start.
+
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    let seconds = DateTime::<Utc>::from(start).timestamp() + 1_000_000_000;
-    let native_date_time = NaiveDateTime::from_timestamp(seconds, 0);
-    return DateTime::<Utc>::from_utc(native_date_time, Utc);
+    start + Duration::seconds(1_000_000_000)
 }
