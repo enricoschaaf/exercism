b141db5c749947096a77f037bd5913b42219ed98
enricoschaaf <enricoschaaf98@gmail.com>
2020-11-20 05:48:51 +0100

    nth prime rust

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,3 +1,10 @@
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    match n {
+        0 => 2,
+        n => (1..)
+            .step_by(2)
+            .filter(|&i| !(2..i / 2 + 1).any(|x| i % x == 0))
+            .nth(n as usize)
+            .unwrap(),
+    }
 }
