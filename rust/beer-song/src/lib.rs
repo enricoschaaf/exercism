09bb73f166b7fa4e58c084d099b1c2cb029203a5
enricoschaaf <enricoschaaf98@gmail.com>
2020-11-20 15:40:08 +0100

    finish beer song  exercise

---
+++ rust/beer-song/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn verse(n: u32) -> String {
+    match n {
+        0 => String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"),
+
+        1 => String::from("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"),
+
+        2 => String::from("2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"),
+
+        n => format!("{current} bottles of beer on the wall, {current} bottles of beer.\nTake one down and pass it around, {next} bottles of beer on the wall.\n", current = n, next = n - 1)
+    }
+}
+
+pub fn sing(start: u32, end: u32) -> String {
+    (end..start + 1)
+        .rev()
+        .map(|x| verse(x))
+        .collect::<Vec<String>>()
+        .join("\n")
+}
