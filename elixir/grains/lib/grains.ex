4f00b5b547e9e5ac6e93b682bdf9c4fe6af1a95e
enricoschaaf <enricoschaaf98@gmail.com>
2020-12-02 13:59:58 +0100

    Elixir Grains

---
+++ elixir/grains/lib/grains.ex
@@ -0,0 +1,21 @@
+defmodule Grains do
+  @doc """
+  Calculate two to the power of the input minus one.
+  """
+  @spec square(pos_integer) :: pos_integer
+  def square(number) when number < 1 or number > 64 do
+    {:error, "The requested square must be between 1 and 64 (inclusive)"}
+  end
+
+  def square(number) do
+    {:ok, :math.pow(2, number - 1) |> round()}
+  end
+
+  @doc """
+  Adds square of each number from 1 to 64.
+  """
+  @spec total :: pos_integer
+  def total do
+    {:ok, 1..64 |> Enum.map(&(square(&1) |> elem(1))) |> Enum.sum()}
+  end
+end
