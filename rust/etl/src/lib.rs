e7f447cfe6108f92bb087f9aae16ed5315bfc902
enricoschaaf <enricoschaaf98@gmail.com>
2020-12-03 11:50:54 +0100

    Rust exercise of the day

---
+++ rust/etl/src/lib.rs
@@ -0,0 +1,10 @@
+use std::collections::BTreeMap;
+
+pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
+    h.iter()
+        .flat_map(|(k, v)| {
+            v.iter()
+                .map(move |c| (c.to_lowercase().nth(0).unwrap(), *k))
+        })
+        .collect()
+}
